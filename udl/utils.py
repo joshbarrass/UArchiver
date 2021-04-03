0cd41d79e6129499fe2c38d299059ac806c407cc
Joshua Barrass <josh.barrass.work@gmail.com>
2021-04-03 02:24:06 +0100

    Function for downloading files with the name provided by the server

--- udl/utils.py
+++ udl/utils.py
@@ -1,6 +1,9 @@
 import os
 import sys
 from distutils.dir_util import copy_tree
+import cgi
+import urllib.parse
+from urllib.request import Request, urlopen, urlretrieve
 
 def strip_http(url):
     """Strip http or https from the front of a url if it exists"""
@@ -22,3 +25,27 @@ def rmdir_or_warn(directory):
             "Warning: Failed to remove directory {}".format(directory),
             file=sys.stderr
         )
+
+def download_original_name(url, outdir=".", useragent=None):
+    """Download a file to a particular directory using the filename
+provided by the server"""
+    if useragent:
+        req = Request(
+            url, data=None, headers={
+                "User-Agent": useragent,
+            }
+        )
+    else:
+        req = url
+
+    # https://stackoverflow.com/a/49733575/7471232
+    remotefile = urlopen(req)
+    blah = remotefile.info()['Content-Disposition']
+    if blah is not None:
+        value, params = cgi.parse_header(blah)
+        filename = params["filename"]
+    else:
+        filename = urllib.parse.unquote_plus(
+            os.path.basename(remotefile.geturl())
+        ).replace(" ", "_")
+    urlretrieve(url, os.path.join(outdir, filename))
